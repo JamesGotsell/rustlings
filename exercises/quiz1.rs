0ab15d4fee4f6bfeb775ab5f7d0c2045fd43c076
James Gotsell <j.e.gotsell@gmail.com>
2023-04-14 18:08:21 +0100

    updated quiiz annd primitive types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,12 +10,11 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+
 
 // Put your function here!
 fn calculate_price_of_apples(number_of_apples: i32) -> i32 {
-
-    if number_of_apples < 40 {
+    if number_of_apples <= 40 {
         return number_of_apples * 2
     }else {
         return number_of_apples
